5a23ca933bb1b12c5eabae60896f434cc1674173
yewon-Noh <yewo2nn16@gmail.com>
2024-04-08 17:38:40 +0900

    2023 알고리즘 풀이 폴더 이동

--- baekjoon/11654_아스키 코드.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args){
-        Scanner in=new Scanner(System.in);
-        char c = in.nextLine().charAt(0);
-        System.out.println((int) c);
-    }
-}
--- baekjoon/7287_등록.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("3");
-        System.out.println("yewo2nn16");
-    }
-}
